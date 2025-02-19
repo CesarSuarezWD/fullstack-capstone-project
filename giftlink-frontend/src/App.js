7bf366e421062b4ca68f1f49294f07836881d5c9
Cesar Suarez <cesar.suarez@indtechcol.com>
2025-02-19 17:49:16 -0500

    [FEATURE] DetailsPage js, css y routing was created

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -4,6 +4,7 @@ import MainPage from './components/MainPage/MainPage';
 import Navbar from './components/Navbar/Navbar';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import LoginPage from './components/LoginPage/LoginPage';
+import DetailsPage from './components/DetailsPage/DetailsPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -18,6 +19,7 @@ function App() {
           <Route path="/app" element={<MainPage />} />
           <Route path="/app/login" element={<LoginPage />} />
           <Route path="/app/register" element={<RegisterPage />} />
+          <Route path="/app/details/:productId" element={<DetailsPage />} />
         </Routes>
         </>
   );
