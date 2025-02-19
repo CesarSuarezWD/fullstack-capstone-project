e79ae1a75f08105010b28585ac239863bbad1b97
Cesar Suarez <cesar.suarez@indtechcol.com>
2025-02-19 18:28:04 -0500

    [FEATURE] SearchPage js, css & routing was created

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar/Navbar';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import DetailsPage from './components/DetailsPage/DetailsPage';
+import SearchPage from './components/SearchPage/SearchPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -20,6 +21,7 @@ function App() {
           <Route path="/app/login" element={<LoginPage />} />
           <Route path="/app/register" element={<RegisterPage />} />
           <Route path="/app/details/:productId" element={<DetailsPage />} />
+          <Route path="/app/search" element={<SearchPage />} />
         </Routes>
         </>
   );
